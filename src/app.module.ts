4a30fa1b23717adc08b8185d0d12637a2a3b4e57
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-07 17:53:15 +0300

    feat: implement connect to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration from '../configuration';
+
 import { ClientsModule } from './modules/clients/clients.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
+      load: [configuration],
     }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${configuration().login}:${
+        configuration().password
+      }@cluster0-k6wuj.mongodb.net/${
+        configuration().dbName
+      }?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
     ClientsModule,
   ],
   controllers: [],
