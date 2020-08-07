7c6c1eec1c5a4e8e77ad78dfd50c747e44188da8
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-07 16:34:43 +0300

    feat: add imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule } from './modules/clients/clients.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ClientsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
