a777fef7414e099618c33b0bcb90a827703e489c
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-07 16:11:40 +0300

    feat: add configService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const conf = app.get(ConfigService);
+  const PORT = conf.get('PORT');
+  await app.listen(PORT);
+  console.log(`Server started on port: ${PORT}`);
 }
 bootstrap();
