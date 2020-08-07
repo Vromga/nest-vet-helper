09061f82d5f3d1849d3abed89c71535bfca87d98
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-07 17:53:15 +0300

    #7 feat: implement connect to mongoDB

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import configuration from '../configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const conf = app.get(ConfigService);
-  const PORT = conf.get('PORT');
-  await app.listen(PORT);
-  console.log(`Server started on port: ${PORT}`);
+  const { port } = configuration();
+  await app.listen(port);
+  console.log(`Server started on port: ${port}`);
 }
 bootstrap();
