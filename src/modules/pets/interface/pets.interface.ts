b8975392d4183181ae97c3ed371c28f14eb9fd46
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-13 17:09:15 +0300

    CRUD for Pets #3: add pets interface

---
+++ src/modules/pets/interface/pets.interface.ts
@@ -0,0 +1,12 @@
+export interface IPets {
+  clientId: string;
+  name: string;
+  age: string;
+  kindOfAnimal: string;
+  breed: string;
+  color: string;
+  chipNumber: string;
+  gender: string;
+  weightAnimal: number;
+  description: string;
+}
