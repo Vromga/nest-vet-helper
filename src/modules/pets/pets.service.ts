3c0c9b41b448dadd5b83e5640324a2e515f4c377
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-13 17:38:37 +0300

    CRUD for Pets #3 feat: add basic pets service structure

--- src/modules/pets/pets.service.ts
+++ src/modules/pets/pets.service.ts
@@ -1,4 +1,20 @@
+import { PetDescriptionOnlyDTO } from './DTO/petDescriptionOnly.dto';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { IPets } from './interface/pets.interface';
+import { Model } from 'mongoose';
+import { PetDTO } from './DTO/pet.dto';
 
 @Injectable()
-export class PetsService {}
+export class PetsService {
+  constructor(@InjectModel('pets') private ps: Model<IPets>) {}
+
+  async getAllPets() {}
+  async getPetById(id: string) {}
+  async getAllPetsByIdOwner(idOwner: string) {}
+  async createPet(idOwner: string, petInfo: IPets) {}
+  async deleteAllPetsByIdOwner(idOwner: string) {}
+  async deletePetById(id: string) {}
+  async patchPetDescription(id: string, description: PetDescriptionOnlyDTO) {}
+  async updatePet(id: string, petInfo: PetDTO) {}
+}
