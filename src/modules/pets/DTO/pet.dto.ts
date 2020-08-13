fa1f14be426243c52fbaf31a7e7d4dd3d3d055f1
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-13 17:34:10 +0300

    CRUD for Pets #3: add pet dto

---
+++ src/modules/pets/DTO/pet.dto.ts
@@ -0,0 +1,13 @@
+export class PetDTO {
+  readonly ownerId: string;
+  readonly name: string;
+  readonly age: string;
+  readonly kindOfAnimal: string;
+  readonly breed: string;
+  readonly color: string;
+  readonly chipNumber: string;
+  readonly gender: string;
+  readonly weightAnimal: number;
+  readonly description: string;
+  readonly isLive: boolean;
+}
