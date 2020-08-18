71e25f43b8edfd3ced84efeee594760a6df0e36e
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-18 16:33:01 +0300

    CRUD for Breeds #4 feat: add method getAll

--- src/modules/breeds/breeds.controller.ts
+++ src/modules/breeds/breeds.controller.ts
@@ -1,6 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { BreedsService } from './breeds.service';
 
 @Controller('breeds')
 export class BreedsController {
-  constructor() {}
+  constructor(private bs: BreedsService) {}
+  @Get()
+  async getAll() {
+    return this.bs.getAll();
+  }
 }
--- src/modules/breeds/breeds.service.ts
+++ src/modules/breeds/breeds.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { IBreed } from './interface/breed.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class BreedsService {
-  constructor() {}
+  constructor(@InjectModel('breed') private bs: Model<IBreed>) {}
+  async getAll() {
+    return this.bs.find();
+  }
 }
