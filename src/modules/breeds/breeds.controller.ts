5a4c4276b9e1b297bfb819db9209a9bba0a5aa30
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-18 17:02:02 +0300

    CRUD for Breeds #4 feat: add get by name method

--- src/modules/breeds/breeds.controller.ts
+++ src/modules/breeds/breeds.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { BreedsService } from './breeds.service';
+import { IBreed } from './interface/breed.interface';
 
 @Controller('breeds')
 export class BreedsController {
   constructor(private bs: BreedsService) {}
   @Get()
-  async getAll() {
-    return this.bs.getAll();
+  async getAll(): Promise<IBreed[]> {
+    return await this.bs.getAll();
+  }
+
+  @Get('/:name')
+  async getByName(@Param('name') name: string): Promise<IBreed> {
+    return await this.bs.getByName(name);
   }
 }
--- src/modules/breeds/breeds.service.ts
+++ src/modules/breeds/breeds.service.ts
@@ -2,11 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { IBreed } from './interface/breed.interface';
 import { Model } from 'mongoose';
+import { BreedDTO } from './DTO/breed.dto';
 
 @Injectable()
 export class BreedsService {
   constructor(@InjectModel('breed') private bs: Model<IBreed>) {}
   async getAll() {
-    return this.bs.find();
+    return await this.bs.find();
+  }
+
+  async getByName(name: string): Promise<IBreed> {
+    return await this.bs.findOne({ name });
+  }
+
+  async creeteBreedCollection(breed: BreedDTO): Promise<IBreed> {
+    const newBreedCollection = new this.bs(breed);
+    return newBreedCollection.save();
   }
 }
