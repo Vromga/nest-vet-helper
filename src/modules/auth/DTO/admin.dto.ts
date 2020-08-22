85bdb35d2318ff40602177cd8bca75f9384abcd1
Mikita Maiseichyk <48679402+Vromga@users.noreply.github.com>
2020-08-22 22:34:32 +0300

    Authorization #19

---
+++ src/modules/auth/DTO/admin.dto.ts
@@ -0,0 +1,7 @@
+export class adminDTO {
+  login: string;
+  passwordHash: string;
+  nickName: string;
+  created_at: Date;
+  updated_at: Date;
+}
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,30 +1,50 @@
-import { AdminRepository } from '../../admin/services/admin.repository';
-import { Admin } from '../../admin/model/admin.model';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { IAdmin, IAadminToResponce } from '../interface/admin.interface';
+import { adminDTO } from '../DTO/admin.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private adminRepository: AdminRepository,
     private JwtService: JwtService,
+    @InjectModel('admin') private as: Model<IAdmin>,
   ) {}
 
-  async validateAdmin(login: string, pass: string): Promise<Admin | null> {
-    const admin: Admin = await this.adminRepository.findByLogin(login);
+  async validateAdmin(
+    login: string,
+    pass: string,
+  ): Promise<IAadminToResponce | null> {
+    const admin: IAdmin = await this.as.findOne({ login });
 
-    if (admin && admin.password === pass) {
-      const { password, ...secureAdmin } = admin;
-      return secureAdmin;
+    if (admin && (await bcrypt.compare(pass, admin.passwordHash))) {
+      const { login, _id, created_at, updated_at, nickName } = admin;
+      return {
+        login,
+        _id,
+        created_at,
+        updated_at,
+        nickName,
+      };
     }
-
     return null;
   }
 
-  async login(admin: Admin) {
-    const payload = { id: admin.id };
+  async login(admin: IAdmin) {
+    const payload = { id: admin._id };
     return {
       accessToken: this.JwtService.sign(payload),
     };
+  }
+
+  async findById(id: string): Promise<IAdmin | undefined> {
+    return this.as.findById({ _id: id });
+  }
+
+  async createAdmin(admin: adminDTO): Promise<IAdmin> {
+    const newAdmin = new this.as({ ...admin });
+    return newAdmin.save();
   }
 }
